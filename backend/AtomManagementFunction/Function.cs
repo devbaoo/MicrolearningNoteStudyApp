df6d7165384bd1c7f3053cfa323392310cffc488
supercoderl <150410325+supercoderl@users.noreply.github.com>
2025-06-01 06:51:50 +0700

    atom-repository

--- backend/AtomManagementFunction/Function.cs
+++ backend/AtomManagementFunction/Function.cs
@@ -1,6 +1,9 @@
 ﻿using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using System.Text.Json;
+using Amazon.DynamoDBv2;
+using Common.Repositories;
+using NeuroBrain.AtomManagementFunction.Services;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -9,6 +12,17 @@ namespace AtomManagementFunction;
 
 public class Function
 {
+
+    private readonly IAtomRepository _atomRepository;
+    private readonly AtomService _atomService;
+
+    public Function()
+    {
+        var dynamoDb = new AmazonDynamoDBClient();
+        _atomRepository = new AtomRepository(dynamoDb);
+        _atomService = new AtomService(_atomRepository);
+    }
+
     public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
         context.Logger.LogInformation("Processing request in AtomManagementFunction");
